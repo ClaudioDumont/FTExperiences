7684020beb0a8cbe67bc9741a001d040c331caec
ClaudioDumont <claudio@chaordicsystems.com>
2018-06-03 08:24:29 -0300

    First attempt at fractal integration

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,39 @@ var gulp = require('gulp'),
 	del = require('del'),
 	babel = require('gulp-babel'),
 	runSequence = require('run-sequence'),
-	browserSync = require('browser-sync').create();
+	browserSync = require('browser-sync').create(),
+	fractal = require('@frctl/fractal').create();
 
+
+//Fractal Config
+
+fractal.set('FTExperiences', 'FooCorp Component Library'); 
+fractal.web.set('builder.dest', 'build'); 
+fractal.docs.set('path', `${__dirname}src/docs`);
+fractal.components.set('path', `${__dirname}src/components`); 
+
+var logger = fractal.cli.console; 
+
+gulp.task('fractal:start', function(){
+    const server = fractal.web.server({
+        sync: true
+    });
+    server.on('error', err => logger.error(err.message));
+    return server.start().then(() => {
+        logger.success(`Fractal server is now running at ${server.url}`);
+    });
+});
+
+gulp.task('fractal:build', function(){
+    const builder = fractal.web.builder();
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
+    return builder.build().then(() => {
+        logger.success('Fractal build completed!');
+    });
+});
+
+//Fractal Config
 
 gulp.task('browser-sync', function(){
 	browserSync.init({
@@ -19,7 +50,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('pug', function buildHTML() {
-  return gulp.src('src/templates/**/*.pug')
+  return gulp.src('src/components/templates/**/*.pug')
 	.pipe(pug({
 		pretty: true
 	}))
@@ -30,13 +61,13 @@ gulp.task('pug', function buildHTML() {
 });
 
 gulp.task('images', function(){
-  return gulp.src('src/assets/images/**/*.+(png|jpg|gif|svg)')
+  return gulp.src('src/components/assets/images/**/*.+(png|jpg|gif|svg)')
   .pipe(imagemin())
   .pipe(gulp.dest('builds/development/assets/images'))
 });
 
 gulp.task('sass', function(){
-	return gulp.src('src/assets/sass/**/*.scss')
+	return gulp.src('src/components/assets/sass/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('builds/development/assets/css'))
 	.pipe(browserSync.reload({
@@ -45,7 +76,7 @@ gulp.task('sass', function(){
 });
 
 gulp.task('scripts', function(){
-	gulp.src('src/assets/js/**/*.js')
+	gulp.src('src/components/assets/js/**/*.js')
 		.pipe(rename({suffix: '.min'}))
 		.pipe(babel({
       		presets: ['env']
@@ -58,10 +89,10 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('watch', ['sass', 'pug', 'images', 'browser-sync'], function(){
-	gulp.watch('src/assets/sass/**/*.scss', ['sass']);
-	gulp.watch('src/templates/**/*.pug', ['pug']);
-	gulp.watch('src/assets/js/**/*.js', ['scripts']);
-	gulp.watch('src/assets/images/**/*.+(png|jpg|gif|svg)', ['images']);
+	gulp.watch('src/components/assets/sass/**/*.scss', ['sass']);
+	gulp.watch('src/components/templates/**/*.pug', ['pug']);
+	gulp.watch('src/components/assets/js/**/*.js', ['scripts']);
+	gulp.watch('src/components/assets/images/**/*.+(png|jpg|gif|svg)', ['images']);
 });
 
 gulp.task('clean:builds', function(){
